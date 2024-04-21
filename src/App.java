7a4f5dff2a38fdd601cf352d38e59c549232ce47
Shrey Kothari <111036307+SHREY29K@users.noreply.github.com>
2024-04-22 00:16:02 +0530

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        TicTacToe tictactoe = new TicTacToe();
+    }
+}
+
+
+//for gameOver Logic
+//while j is moving and i is constant
+//matrix[i][j] || matrix[i][j]
+
+//when i and j are both moving
+//matric[i][j]
